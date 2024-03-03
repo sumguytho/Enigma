f9486ae190344f26cbd19dffd78d79c8355af041
sumguytho <sumguytho@gmail.com>
2024-03-03 18:21:32 +0300

    copyDeps automatically

--- launch.sh
+++ launch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if [ ! -d "enigma-swing/build/jarsfolder" ]; then
+    ./gradlew copyDeps
+fi
 ./addsyms.sh
 cd enigma-swing/build/jarsfolder
 java -cp $(../../../collectcp.sh) cuchaz.enigma.gui.Main
